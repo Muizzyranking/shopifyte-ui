70e82d35053ea8fcd3b5708e23d4d0660144f3e2
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-09-08 02:26:41 +0100

    feat: types

---
+++ src/types/auth.ts
@@ -0,0 +1,9 @@
+export interface RegisterData {
+  email: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  password: string;
+  confirmPassword: string;
+}
