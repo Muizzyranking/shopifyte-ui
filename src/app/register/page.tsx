0a95aeab0e7cb5a0627b1130a1cf961239d1e494
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-09-28 02:51:26 +0100

    feat: registeration page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,17 +1,19 @@
-import { GalleryVerticalEnd } from "lucide-react";
-import RegistrationForm from "@/components/registerationForm";
+"use client";
+import { useRouter } from "next/navigation";
+import { RegisterForm } from "./RegisterForm";
 
 export default function RegisterationPage() {
+  const router = useRouter();
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-md md:max-w-lg lg:max-w-xl flex-col gap-6">
-        <a href="/" className="flex items-center gap-2 self-center font-medium">
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
-          </div>
-          Shopifyte
-        </a>
-        <RegistrationForm />
+    <div className=" flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <RegisterForm
+          onSuccess={(e) => {
+            router.push(
+              `/registeration-success?${new URLSearchParams({ email: e.email }).toString()}`,
+            );
+          }}
+        />
       </div>
     </div>
   );
