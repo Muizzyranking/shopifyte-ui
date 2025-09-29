d305adbcbb60bc3b70fa32586dff4a0f3a944a45
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-09-29 19:50:55 +0100

    refactor: do not set email in the path

--- src/app/registeration-success/page.tsx
+++ src/app/registeration-success/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import RegistrationSuccessPage from "./RegisterationSuccessPage";
 
 export default function RegisterationSuccess() {
-  const searchParams = useSearchParams();
-  const email = searchParams.get("email");
+  const [email, setEmail] = useState<string | null>(null);
+  useEffect(() => {
+    const registeredEmail = sessionStorage.getItem("registeredEmail");
+    if (registeredEmail) {
+      setEmail(registeredEmail);
+      sessionStorage.removeItem("registeredEmail");
+    }
+  });
   return <RegistrationSuccessPage email={email || undefined} />;
 }
